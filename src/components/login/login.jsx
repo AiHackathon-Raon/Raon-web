bfbbec8bd4707e562eb739a3a4de92769f769405
권예림 <124475716+yerim07@users.noreply.github.com>
2023-08-01 22:26:24 +0900

    font-family
    왜 안되누,,

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import BackGround from "../../asset/img/background.svg";
+import BackGround from "../../asset/img/background_f.svg";
 import * as L from "./login.style";
 
 function Login() {
   return (
     <>
       <L.Img src={BackGround}></L.Img>
+      <L.Back></L.Back>
+      <L.Logo>Raon</L.Logo>
     </>
   );
 }
--- src/components/login/login.style.js
+++ src/components/login/login.style.js
@@ -1,14 +1,33 @@
 import styled from "styled-components";
 
 export const Img = styled.img`
-  top: 0;
-  bottom: 0;
-  width: 100%;
-  height: 100%;
-  margin: 0px;
-  padding: 0px;
+  z-index: -1;
+  width: 100vw;
+  height: 100vh;
   background-repeat: no-repeat;
   background-size: cover;
   position: absolute;
-  z-index: 11;
+`;
+
+export const Back = styled.div`
+  z-index: 1;
+  width: 600px;
+  height: 700px;
+  flex-shrink: 0;
+  border-radius: 50px;
+  backdrop-filter: blur(5px);
+  background: rgba(34, 61, 174, 0.17);
+  transform: translate(90%, 17%);
+`;
+
+export const Logo = styled.div`
+  z-index: 2;
+  color: #223dae;
+  text-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.25);
+  font-family: "Rubik Glitch", sans-serif;
+  font-size: 90px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  transform: translate(44%, -510%);
 `;
